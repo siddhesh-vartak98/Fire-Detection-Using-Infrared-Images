5726add359ad70c06628a472916d21691374393a
siddhesh-vartak98 <56190795+siddhesh-vartak98@users.noreply.github.com>
2020-02-10 15:46:07 +0530

    Add files via upload

---
+++ AForge.Imaging/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// Assembly AForge.Imaging, Version 2.1.5.0
+
+[assembly: System.Reflection.AssemblyVersion("2.1.5.0")]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Reflection.AssemblyCompany("AForge")]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Reflection.AssemblyCopyright("AForge \x00a9 2011")]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyProduct("AForge.NET")]
+[assembly: System.Reflection.AssemblyTitle("AForge.Imaging")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("0c6c72f4-7281-4822-9414-cc0758a88abc")]
+[assembly: System.Reflection.AssemblyFileVersion("2.1.5.0")]
+[assembly: System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.RequestMinimum, SkipVerification=true)]
+
